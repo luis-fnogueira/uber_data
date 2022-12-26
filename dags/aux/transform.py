b039ceca8fd5363e4f8a0b774f786932a62c678b
Luis Felipe <luis.felipe32971@gmail.com>
2022-12-26 18:57:54 -0300

    removed load and csv file

--- dags/aux/transform.py
+++ dags/aux/transform.py
@@ -1,3 +1,4 @@
+import re
 import pandas as pd
 
 class Transform:
@@ -40,3 +41,38 @@ class Transform:
 
             # Removing non completed trips
             dataframe.drop(dataframe[dataframe['Trip or Order Status'] != "COMPLETED"].index, inplace = True)
+
+
+    @staticmethod
+    def types_all_upper(dataframe: pd.DataFrame) -> None:
+
+        """
+        Turning every Product Type in Upper case, so all UberX rides fit in the same category
+        
+        Args:
+            Dataframe: pd.DataFrame. The Dataframe that'll be changed
+        Returns:
+            Changes every product type to upper case
+        """
+
+        dataframe['Product Type'] = dataframe['Product Type'].apply(lambda s: str.upper(s))
+
+
+    @staticmethod
+    def check_city(dataframe: pd.DataFrame) -> None:
+
+        """
+        This functions checks if there is the string "Jundiaí" in the Address column.
+        If true, it'll change the city column to Jundiaí as well.
+        
+        Args:
+            Dataframe: pd.DataFrame. The Dataframe that'll be changed
+        Returns:
+            Changes the City column based on a condition
+        """
+
+
+        for index in dataframe.index:
+
+            if re.search("Jundiaí", dataframe.loc[index, "Dropoff Address"]):
+                dataframe.loc[index, "City"] = "Jundiaí"
